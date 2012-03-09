755a480a08d1febc9cff855fc7158411c8ca3417
Les Hill <leshill@gmail.com>
2012-03-08 22:26:24 -0800

    Cleanup

--- lib/hogan_assets/config.rb
+++ lib/hogan_assets/config.rb
@@ -8,14 +8,14 @@ module HoganAssets
   # HoganAssets.configure do |config|
   #   config.template_extension = 'mustache'
   # end
-  
+
   module Config
     attr_accessor :template_base_path, :template_extension
-    
+
     def configure
       yield self
     end
-    
+
     def template_extension
       @template_extension ||= 'mustache'
     end
