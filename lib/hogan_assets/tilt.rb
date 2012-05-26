176d52a2318fab394dd38132873f5e18bce03448
Mike Smullin <mike@smullindesign.com>
2012-05-26 10:53:50 -0600

    attempting to get hogan to compile haml as well

--- lib/hogan_assets/tilt.rb
+++ lib/hogan_assets/tilt.rb
@@ -4,9 +4,24 @@ module HoganAssets
   class Tilt < Tilt::Template
     self.default_mime_type = 'application/javascript'
 
+    def self.engine_initialized?
+      defined? ::Haml::Engine
+    end
+
+    def initialize_engine
+      require_template_library 'haml'
+    end
+
     def evaluate(scope, locals, &block)
+
+require 'pry'
+binding.pry
+
       compiled_template = Hogan.compile(data)
       template_name = scope.logical_path.inspect
+      options = @options.merge(:filename => eval_file, :line => line)
+      Haml::Engine.new(compiled_template, options).render(scope)
+
       <<-TEMPLATE
         this.HoganTemplates || (this.HoganTemplates = {});
         this.HoganTemplates[#{template_name}] = new Hogan.Template(#{compiled_template});
