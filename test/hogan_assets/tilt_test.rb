5cd5955354d50c4da5db06beeef94675c8272871
Les Hill <leshill@gmail.com>
2012-08-04 17:23:43 -0700

    Refactor sprockets mock

--- test/hogan_assets/tilt_test.rb
+++ test/hogan_assets/tilt_test.rb
@@ -2,16 +2,17 @@ require 'test_helper'
 
 module HoganAssets
   class TiltTest < Test::Unit::TestCase
-    def scope(path)
-      instance = Class.new do
-        attr_accessor :_path
+   # Try to act like sprockets.
+    def make_scope(root, file)
+      Class.new do
+        define_method(:logical_path) { pathname.to_s.gsub(root + '/', '').gsub(/\..*/, '') }
 
-        def logical_path ; _path.gsub /\..*/, '' ; end
+        define_method(:pathname) { Pathname.new(root) + file }
 
-        def pathname ; Pathname.new _path ; end
+        define_method(:root_path) { root }
+
+        define_method(:s_path) { pathname.to_s }
       end.new
-      instance._path = path
-      instance
     end
 
     def test_mime_type
@@ -19,28 +20,35 @@ module HoganAssets
     end
 
     def test_render
-      path = 'path/to/template.mustache'
-      template = HoganAssets::Tilt.new(path) { "This is {{mustache}}" }
-      assert_equal <<-END_EXPECTED, template.render(scope(path), {})
+      scope = make_scope '/myapp/app/assets/javascripts', 'path/to/template.mustache'
+
+      template = HoganAssets::Tilt.new(scope.s_path) { "This is {{mustache}}" }
+
+      assert_equal <<-END_EXPECTED, template.render(scope, {})
         this.HoganTemplates || (this.HoganTemplates = {});
         this.HoganTemplates[\"path/to/template\"] = new Hogan.Template({code: function (c,p,i) { var t=this;t.b(i=i||\"\");t.b(\"This is \");t.b(t.v(t.f(\"mustache\",c,p,0)));return t.fl(); },partials: {}, subs: {  }}, "", Hogan, {});
       END_EXPECTED
     end
 
     def test_hamstache_render
-      path = 'path/to/template.hamstache'
-      template = HoganAssets::Tilt.new(path) { "%p This is {{hamstache}}" }
-      assert_equal <<-END_EXPECTED, template.render(scope(path), {})
+      scope = make_scope '/myapp/app/assets/javascripts', 'path/to/template.hamstache'
+
+      template = HoganAssets::Tilt.new(scope.s_path) { "%p This is {{hamstache}}" }
+
+      assert_equal <<-END_EXPECTED, template.render(scope, {})
         this.HoganTemplates || (this.HoganTemplates = {});
         this.HoganTemplates[\"path/to/template\"] = new Hogan.Template({code: function (c,p,i) { var t=this;t.b(i=i||\"\");t.b(\"<p>This is \");t.b(t.v(t.f(\"hamstache\",c,p,0)));t.b(\"</p>\");t.b(\"\\n\");return t.fl(); },partials: {}, subs: {  }}, \"\", Hogan, {});
       END_EXPECTED
     end
 
     def test_render_with_lambdas
-      path = 'path/to/template.mustache'
       HoganAssets::Config.lambda_support = true
-      template = HoganAssets::Tilt.new(path) { "This is {{mustache}}" }
-      assert_equal <<-END_EXPECTED, template.render(scope(path), {})
+
+      scope = make_scope '/myapp/app/assets/javascripts', 'path/to/template.mustache'
+
+      template = HoganAssets::Tilt.new(scope.s_path) { "This is {{mustache}}" }
+
+      assert_equal <<-END_EXPECTED, template.render(scope, {})
         this.HoganTemplates || (this.HoganTemplates = {});
         this.HoganTemplates[\"path/to/template\"] = new Hogan.Template({code: function (c,p,i) { var t=this;t.b(i=i||\"\");t.b(\"This is \");t.b(t.v(t.f(\"mustache\",c,p,0)));return t.fl(); },partials: {}, subs: {  }}, "This is {{mustache}}", Hogan, {});
       END_EXPECTED
