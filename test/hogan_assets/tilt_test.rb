353d4d03da297b3553feaa7aa4ce5cb22c688080
Les Hill <leshill@gmail.com>
2013-02-06 14:52:30 -0800

    Extract make_scope to TestSupport module

--- test/hogan_assets/tilt_test.rb
+++ test/hogan_assets/tilt_test.rb
@@ -2,18 +2,7 @@ require 'test_helper'
 
 module HoganAssets
   class TiltTest < Test::Unit::TestCase
-   # Try to act like sprockets.
-    def make_scope(root, file)
-      Class.new do
-        define_method(:logical_path) { pathname.to_s.gsub(root + '/', '').gsub(/\..*/, '') }
-
-        define_method(:pathname) { Pathname.new(root) + file }
-
-        define_method(:root_path) { root }
-
-        define_method(:s_path) { pathname.to_s }
-      end.new
-    end
+    include TestSupport
 
     def teardown
       HoganAssets::Config.lambda_support = false
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,18 @@ require 'hogan_assets/hogan'
 require 'hogan_assets/config'
 
 require 'test/unit'
+
+module TestSupport
+  # Try to act like sprockets.
+  def make_scope(root, file)
+    Class.new do
+      define_method(:logical_path) { pathname.to_s.gsub(root + '/', '').gsub(/\..*/, '') }
+
+      define_method(:pathname) { Pathname.new(root) + file }
+
+      define_method(:root_path) { root }
+
+      define_method(:s_path) { pathname.to_s }
+    end.new
+  end
+end
